60f10e85b466c1906c568d92f4a4415fd8287b0e
Manish Kumar <manish.kumar@setuserv.com>
2020-02-21 22:07:08 +0530

    Improved memory usage

--- Practice/longestCommonSubsequence/lcs.go
+++ Practice/longestCommonSubsequence/lcs.go
@@ -1,9 +1,5 @@
 package main
 
-type Pair struct {
-	i, j int
-}
-
 func max(x, y int) int {
 	if x < y {
 		return y
@@ -11,20 +7,13 @@ func max(x, y int) int {
 	return x
 }
 
-func lcsHelper(text1 string, text2 string, index1 int, index2 int, lcsMap map[Pair]int) int {
-	if index1 >= len(text1) {
-		return 0
-	}
-	if index2 >= len(text2) {
+func lcsHelper(text1 string, text2 string, index1 int, index2 int, lcsMap [][]int) int {
+	if index1 >= len(text1) || index2 >= len(text2) {
 		return 0
 	}
 
-	value, ok := lcsMap[Pair{
-		i: index1,
-		j: index2,
-	}]
-
-	if ok {
+	value := lcsMap[index1][index2]
+	if value >= 0 {
 		return value
 	}
 
@@ -34,13 +23,21 @@ func lcsHelper(text1 string, text2 string, index1 int, index2 int, lcsMap map[Pa
 		value = max(lcsHelper(text1, text2, index1, index2+1, lcsMap), lcsHelper(text1, text2, index1+1, index2, lcsMap))
 	}
 
-	lcsMap[Pair{index1, index2}] = value
+	lcsMap[index1][index2] = value
 
 	return value
 }
 
 func longestCommonSubsequence(text1 string, text2 string) int {
-	lcsMap := make(map[Pair]int)
+	lcsMap := make([][]int, len(text1))
+	for i := 0; i < len(text1); i++ {
+		lcsMap[i] = make([]int, len(text2))
+	}
 
+	for i := range lcsMap {
+		for j := range lcsMap[i] {
+			lcsMap[i][j] = -1
+		}
+	}
 	return lcsHelper(text1, text2, 0, 0, lcsMap)
 }
