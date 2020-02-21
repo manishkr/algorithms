98227a4372e3a8198235e9c2843f623ff2e5ecb2
Manish Kumar <manish.kumar@setuserv.com>
2020-02-21 22:45:55 +0530

    Fixed test check

--- Practice/longestCommonSubsequence/lcs_test.go
+++ Practice/longestCommonSubsequence/lcs_test.go
@@ -35,22 +35,22 @@ func TestLCS(t *testing.T) {
 		t.Errorf("longestCommonSubsequence(input41, input42) = %d; want 5", output)
 	}
 
-	output = longestCommonSubsequence(input11, input12)
+	output = longestCommonSubsequenceBottomUp(input11, input12)
 	if output != 3 {
 		t.Errorf("longestCommonSubsequenceBottomUp(input11, input12) = %d; want 3", output)
 	}
 
-	output = longestCommonSubsequence(input21, input22)
+	output = longestCommonSubsequenceBottomUp(input21, input22)
 	if output != 3 {
 		t.Errorf("longestCommonSubsequenceBottomUp(input21, input22) = %d; want 3", output)
 	}
 
-	output = longestCommonSubsequence(input31, input32)
+	output = longestCommonSubsequenceBottomUp(input31, input32)
 	if output != 0 {
 		t.Errorf("longestCommonSubsequenceBottomUp(input31, input32) = %d; want 0", output)
 	}
 
-	output = longestCommonSubsequence(input41, input42)
+	output = longestCommonSubsequenceBottomUp(input41, input42)
 	if output != 5 {
 		t.Errorf("longestCommonSubsequenceBottomUp(input41, input42) = %d; want 5", output)
 	}
