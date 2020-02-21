abc89488d08402d3e512909d627d80a5d4503e44
Manish Kumar <manish.kumar@setuserv.com>
2020-02-21 22:51:32 +0530

    Optimized space

--- Practice/longestCommonSubsequence/lcsBottomUp.go
+++ Practice/longestCommonSubsequence/lcsBottomUp.go
@@ -1,21 +1,22 @@
 package main
 
 func longestCommonSubsequenceBottomUp(text1 string, text2 string) int {
-	lcsMap := make([][]int, len(text1)+1)
-	for i := 0; i < len(text1)+1; i++ {
+	lcsMap := make([][]int, 2)
+	for i := 0; i < 2; i++ {
 		lcsMap[i] = make([]int, len(text2)+1)
 	}
-
+	bi := 0
 	for i := 0; i <= len(text1); i++ {
+		bi = i & 1
 		for j := 0; j <= len(text2); j++ {
 			if i != 0 && j != 0 {
 				if text1[i-1] == text2[j-1] {
-					lcsMap[i][j] = 1 + lcsMap[i-1][j-1]
+					lcsMap[bi][j] = 1 + lcsMap[1-bi][j-1]
 				} else {
-					lcsMap[i][j] = max(lcsMap[i][j-1], lcsMap[i-1][j])
+					lcsMap[bi][j] = max(lcsMap[bi][j-1], lcsMap[1-bi][j])
 				}
 			}
 		}
 	}
-	return lcsMap[len(text1)][len(text2)]
+	return lcsMap[bi][len(text2)]
 }
