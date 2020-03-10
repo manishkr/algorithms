f75911b676f60478bab21307224831f30da74e9a
Manish Kumar <manish.kumar@setuserv.com>
2020-03-10 23:29:34 +0530

    Added basic solution

--- Practice/longestSubstring/lengthOfLongestSubstring.go
+++ Practice/longestSubstring/lengthOfLongestSubstring.go
@@ -1,5 +1,41 @@
 package main
 
+import (
+	"strings"
+)
+
+func max(x, y int) int {
+	if x < y {
+		return y
+	}
+	return x
+}
+
+func createSubString(s []string, index int) int {
+	set := make(map[string]bool)
+
+	for i := index; i < len(s); i++ {
+		if _, ok := set[s[i]]; ok {
+			return len(set)
+		} else {
+			set[s[i]] = true
+		}
+	}
+
+	return len(set)
+}
+
 func lengthOfLongestSubstring(s string) int {
-	return 0
+	lsMap := make([]map[string]bool, len(s))
+	for i := 0; i < len(s); i++ {
+		lsMap[i] = make(map[string]bool)
+	}
+
+	strArray := strings.Split(s, "")
+	longestLength := 0
+	for i := 0; i < len(s); i++ {
+		longestLength = max(longestLength, createSubString(strArray, i))
+	}
+
+	return longestLength
 }
--- Practice/longestSubstring/lengthOfLongestSubstring_test.go
+++ Practice/longestSubstring/lengthOfLongestSubstring_test.go
@@ -6,18 +6,18 @@ func TestLenghtOfLongestSubstring(t *testing.T) {
 	input := "abcabcbb"
 	output := lengthOfLongestSubstring(input)
 	if output != 3 {
-		t.Errorf("lengthOfLongestSubstring(input) = %d; want 3", output)
+		t.Errorf("lengthOfLongestSubstring(%s) = %d; want 3", input, output)
 	}
 
 	input = "bbbbb"
 	output = lengthOfLongestSubstring(input)
 	if output != 1 {
-		t.Errorf("lengthOfLongestSubstring(input) = %d; want 1", output)
+		t.Errorf("lengthOfLongestSubstring(%s) = %d; want 1", input, output)
 	}
 
 	input = "pwwkew"
 	output = lengthOfLongestSubstring(input)
 	if output != 3 {
-		t.Errorf("lengthOfLongestSubstring(input) = %d; want 3", output)
+		t.Errorf("lengthOfLongestSubstring(%s) = %d; want 3", input, output)
 	}
 }
