079f563d4d34be382277474c5ce31d0516ce35be
Manish Kumar <manis.kumar@gmail.com>
2020-04-11 17:12:44 +0530

    DP solution for rain water

--- Practice/trappingRainWater/trappingRainWater.go
+++ Practice/trappingRainWater/trappingRainWater.go
@@ -15,16 +15,25 @@ func min(x int, y int) int {
 	return y
 }
 func trap(height []int) int {
+	size := len(height)
+	if size == 0 {
+		return 0
+	}
 	trapArea := 0
-	for i := 0; i < len(height); i++ {
-		leftMax, rightMax := 0, 0
-		for j := 0; j < i; j++ {
-			leftMax = max(leftMax, height[j])
-		}
-		for j := i + 1; j < len(height); j++ {
-			rightMax = max(rightMax, height[j])
-		}
-		area := min(leftMax, rightMax) - height[i]
+	left := make([]int, len(height))
+	right := make([]int, len(height))
+	left[0] = height[0]
+	right[size-1] = height[size-1]
+
+	for i := 1; i < len(height); i++ {
+		left[i] = max(height[i], left[i-1])
+	}
+	for i := size - 2; i > 0; i-- {
+		right[i] = max(height[i], right[i+1])
+	}
+
+	for i := 0; i < size; i++ {
+		area := min(left[i], right[i]) - height[i]
 		if area > 0 {
 			trapArea += area
 		}
