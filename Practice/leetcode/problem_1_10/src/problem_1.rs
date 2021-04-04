57c3d75abf487a612d96cccb50db357c29c24467
Manish Kumar <manis.kumar@gmail.com>
2021-04-04 18:29:06 +0530

    Added problem 1 solution

---
+++ Practice/leetcode/problem_1_10/src/problem_1.rs
@@ -0,0 +1,13 @@
+pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    let mut v: Vec<i32> = Vec::new();
+    for (pos_1, e_1) in nums.iter().enumerate() {
+       for (pos_2, e_2) in nums[pos_1+1..].iter().enumerate() {
+           if e_1 + e_2 == target {
+               v.push(pos_1 as i32);
+               v.push((pos_1 + 1 + pos_2) as i32);
+               break;
+           }
+       }
+    }
+    return v;
+}
