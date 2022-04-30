012fbbd5e4f25caa92ed4101f04fdb146264a067
Manish Kumar <manis.kumar@gmail.com>
2022-04-30 21:40:35 +0530

    Added median of sorted arrays solution

--- Practice/leetcode/problem_1_10/src/lib.rs
+++ Practice/leetcode/problem_1_10/src/lib.rs
@@ -1,3 +1,4 @@
 pub mod two_sum;
 pub mod add_two_numbers;
 pub mod length_of_longest_substring;
+pub mod median_of_sorted_arrays;
---
+++ Practice/leetcode/problem_1_10/tests/median_of_sorted_arrays_test.rs
@@ -0,0 +1,31 @@
+#[cfg(test)]
+mod tests {
+  use problem_1_10::median_of_sorted_arrays;
+
+  #[test]
+  fn median_of_sorted_arrays_1() {
+    assert_eq!(median_of_sorted_arrays::find_median_sorted_arrays(vec![1, 3], vec![2]), 2.0);
+  }
+
+  #[test]
+  fn median_of_sorted_arrays_2() {
+    assert_eq!(median_of_sorted_arrays::find_median_sorted_arrays(vec![1, 2], vec![3, 4]), 2.5);
+  }
+
+  #[test]
+  fn median_of_sorted_arrays_3() {
+    assert_eq!(median_of_sorted_arrays::find_median_sorted_arrays(vec![1, 2, 3, 4, 5, 6, 7, 8],
+                                                                  vec![1, 2, 3, 4]), 3.5);
+  }
+
+  #[test]
+  fn median_of_sorted_arrays_4() {
+    assert_eq!(median_of_sorted_arrays::find_median_sorted_arrays(vec![1], vec![]), 1.0);
+  }
+
+  #[test]
+  fn median_of_sorted_arrays_5() {
+    assert_eq!(median_of_sorted_arrays::find_median_sorted_arrays(vec![], vec![2]), 2.0);
+  }
+}
+
