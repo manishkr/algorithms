42a676862431db12bcba5d43fa4a94a6cf6f54d7
Manish Kumar <manis.kumar@gmail.com>
2022-05-02 19:44:54 +0530

    Added longest palindromic substring solution

--- Practice/leetcode/problem_1_10/src/lib.rs
+++ Practice/leetcode/problem_1_10/src/lib.rs
@@ -2,3 +2,4 @@ pub mod two_sum;
 pub mod add_two_numbers;
 pub mod length_of_longest_substring;
 pub mod median_of_sorted_arrays;
+pub mod longest_palindromic_substring;
---
+++ Practice/leetcode/problem_1_10/tests/longest_palindromic_substring_test.rs
@@ -0,0 +1,39 @@
+#[cfg(test)]
+mod tests {
+  use problem_1_10::longest_palindromic_substring;
+
+  #[test]
+  fn test_longest_palindromic_substring_1() {
+    assert_eq!(longest_palindromic_substring::longest_palindrome("".to_owned()), "");
+  }
+
+  #[test]
+  fn test_longest_palindromic_substring_2() {
+    assert_eq!(longest_palindromic_substring::longest_palindrome("babad".to_owned()), "bab");
+  }
+
+  #[test]
+  fn test_longest_palindromic_substring_3() {
+    assert_eq!(longest_palindromic_substring::longest_palindrome("cbbd".to_owned()), "bb");
+  }
+
+  #[test]
+  fn test_longest_palindromic_substring_4() {
+    assert_eq!(longest_palindromic_substring::longest_palindrome("bbbb".to_owned()), "bbbb");
+  }
+
+  #[test]
+  fn test_longest_palindromic_substring_5() {
+    assert_eq!(longest_palindromic_substring::longest_palindrome("pqrstuv".to_owned()), "p");
+  }
+
+  #[test]
+  fn test_longest_palindromic_substring_6() {
+    assert_eq!(longest_palindromic_substring::longest_palindrome("ccc".to_owned()), "ccc");
+  }
+
+  #[test]
+  fn test_longest_palindromic_substring_7() {
+    assert_eq!(longest_palindromic_substring::longest_palindrome("dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd".to_owned()), "dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd");
+  }
+}
