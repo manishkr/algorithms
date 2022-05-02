629729a7bef8a2d621b374e24ba2d61043090127
Manish Kumar <manis.kumar@gmail.com>
2022-05-02 22:41:34 +0530

    Added zigzag conversion solution

--- Practice/leetcode/problem_1_10/src/lib.rs
+++ Practice/leetcode/problem_1_10/src/lib.rs
@@ -3,3 +3,4 @@ pub mod add_two_numbers;
 pub mod length_of_longest_substring;
 pub mod median_of_sorted_arrays;
 pub mod longest_palindromic_substring;
+pub mod zigzag_conversion;
---
+++ Practice/leetcode/problem_1_10/src/zigzag_conversion.rs
@@ -0,0 +1,18 @@
+pub fn convert(s: String, num_rows: i32) -> String {
+  if num_rows == 1 || s.len() < num_rows as usize {
+    return s;
+  }
+  let mut rows: Vec<String> = vec![String::new(); s.len()];
+  let mut reverse = false;
+  let mut row = 0i32;
+  let last_row = num_rows - 1;
+
+  for i in 0..s.len() {
+    rows[row as usize] += &s[i..=i];
+    let inc = if reverse { -1i32 } else { 1i32 };
+    row += inc;
+    reverse = reverse ^ (row == last_row || row == 0);
+  }
+
+  return rows.into_iter().collect();
+}
